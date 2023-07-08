4b250519c22384d0e364d6662b211e7b97a5e659
Cyril de Catheu <cyril@startree.ai>
2023-07-08 19:35:58 +0200

    [distribution] make jar executable

---
+++ jnotebook-distribution/packaging/cross_platform_exec.sh
@@ -0,0 +1,17 @@
+:;if ! command -v java &>/dev/null; then
+:;  echo "java is not found in the path. Java is not installed?"
+:;  exit 1
+:;fi
+:;java_version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+:;major_version=$(echo "$java_version" | awk -F '.' '{print $1}')
+:;if [ "$major_version" -ge 17 ]; then
+:;  echo "Using java $java_version"
+:;else
+:;  echo "java version is $major_version. jnotebook requires java >= 17."
+:;  exit 1
+:;fi
+:;JNOTEBOOK_JAVA_OPTS=${JNOTEBOOK_JAVA_OPTS:-""}
+:;exec java $JNOTEBOOK_JAVA_OPTS -jar $0 "$@"
+
+:; # support for windows is limited -
+@echo off\r\njava -jar %%1 \"%%~f0\" %%*\r\ngoto :eof
