83641f6f7c87f319a7ae75d1c39612d2cba9c340
Cyril de Catheu <cyril@startree.ai>
2023-07-15 12:29:34 +0200

    [core] improve first launch experience (#8)

--- jnotebook-core/src/main/java/tech/catheu/jnotebook/localstorage/LocalStorage.java
+++ jnotebook-core/src/main/java/tech/catheu/jnotebook/localstorage/LocalStorage.java
@@ -17,17 +17,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.catheu.jnotebook.Main;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Locale;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkState;
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static tech.catheu.jnotebook.Constants.VERSION;
+import static tech.catheu.jnotebook.utils.FileUtils.createDirectoriesIfNotExists;
+import static tech.catheu.jnotebook.utils.FileUtils.writeResourceToFile;
 
 public class LocalStorage {
 
@@ -64,35 +62,10 @@ public class LocalStorage {
     final boolean isInLocalStorage = Files.exists(localStorageUtilsJarPath);
     if (isSnapshot || !isInLocalStorage) {
       LOG.info("Copying {} to local storage", localStorageUtilsJarName);
-      copyResourcesToFile(RESOURCES_JNOTEBOOK_UTILS_PATH,
-                          localStorageUtilsJarPath);
+      createDirectoriesIfNotExists(localStorageUtilsJarPath.getParent());
+      writeResourceToFile(RESOURCES_JNOTEBOOK_UTILS_PATH,
+                         localStorageUtilsJarPath);
     }
     return localStorageUtilsJarPath.toString();
   }
-
-  private static void copyResourcesToFile(final String resourcesPath, final Path filePath) {
-    try (final InputStream in = LocalStorage.class.getResourceAsStream(resourcesPath)) {
-      checkState(in != null, "Failed reading %s from resources", resourcesPath);
-      createIfNotExists(filePath.getParent());
-      Files.copy(in, filePath, REPLACE_EXISTING);
-    } catch (IOException e) {
-      throw new RuntimeException(java.lang.String.format("Failed copying resource %s to file %s: %s",
-                                                         resourcesPath,
-                                                         filePath,
-                                                         e));
-    }
-  }
-
-  private static void createIfNotExists(final Path folderPath) {
-    try {
-      if (!Files.exists(folderPath)) {
-        LOG.info("{} folder does not exist. Creating folder.", folderPath);
-        Files.createDirectories(folderPath);
-      }
-    } catch (Exception e) {
-      LOG.error("Failed creating folder {}. Error: {}", folderPath, e.getMessage());
-      throw new RuntimeException(e); // don't try to recover if the system is not able to create folders
-    }
-  }
-
 }
---
+++ jnotebook-core/src/main/resources/jnb_interactive/hello_world.jsh
@@ -0,0 +1,16 @@
+// # Hello world
+// Cells are delimited by blank lines
+
+String s1 = "Hello";
+
+String s2 = "World";
+
+// for multi-statements cells, only the last value or method result is returned.
+
+String s3 = "!";
+String greeting = s1 + " " + s2 + s3;
+
+// ## markdown
+// ### is
+// #### supported
+// Learn more on [jnotebook.catheu.tech](https://jnotebook.catheu.tech)
