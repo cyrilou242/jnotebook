9ed6492b6a606979eba3921a381e803ae88423b3
Cyril de Catheu <cyril@startree.ai>
2023-11-04 14:56:54 +0100

    change license to MIT

--- jnotebook-core/src/main/java/tech/catheu/jnotebook/server/HtmlTemplateEngine.java
+++ jnotebook-core/src/main/java/tech/catheu/jnotebook/server/HtmlTemplateEngine.java
@@ -1,15 +1,9 @@
 /*
- * Copyright Cyril de Catheu, 2023
+ * Copyright 2023 Cyril de Catheu
  *
- * Licensed under the JNOTEBOOK LICENSE 1.0 (the "License"); you may not use
- * this file except in compliance with the License. You may obtain a copy of the
- * License at https://raw.githubusercontent.com/cyrilou242/jnotebook/main/LICENSE
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT * WARRANTIES OF ANY KIND,
- * either express or implied.
- * See the License for the specific language governing permissions and limitations under
- * the License.
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
  */
 package tech.catheu.jnotebook.server;
 
